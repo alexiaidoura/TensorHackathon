79268f7465e4adb7ad0cf077fa9a9e0cd4ebb75c
katelynmhopper <katelynmhopper@gmail.com>
2022-07-12 14:42:20 -0600

    About page complete updated Navbar design

--- pubsafety-app/src/components/About.js
+++ pubsafety-app/src/components/About.js
@@ -3,13 +3,25 @@ function About(){
       <div class = "about">
       <form>
       <div class = "info"> 
+      Whoville City Building<br></br>
+      1234 S 2735 W <br></br>
+      Whoville, Ut, 12345 <br></br>
+
+      Hours: <br></br>
+      Monday - Friday 8:00-5:00 <br></br>
+      <br/>
+
         Contact Information <br></br>
       Email Address: whoville@gmail.com <br></br>
       Phone: 123-456-7890 <br></br>
-
+      <br/>
+      
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address: </label>
+        <label for="exampleFormControlInput1" class="form-label">
+          Please submit any questions, comments or concerns about community events below. <br></br>
+          Email address: 
+          </label>
         <input type="email" class="form-control" id="exampleFormControlInput1"/>
       </div>
       <div class="mb-3">
