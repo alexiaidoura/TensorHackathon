54b5cf7ddd900b86aa1925d6d8499854444d4058
katelynmhopper <katelynmhopper@gmail.com>
2022-07-12 13:03:54 -0600

    About page formatting

--- pubsafety-app/src/components/About.js
+++ pubsafety-app/src/components/About.js
@@ -1,12 +1,24 @@
 function About(){
     return (
-     <Card 
-        bgcolor="gray"
-        txtcolor="black"
-        header="HEADER"
-        title="TITLE"
-        text="BLAH BLAH"
-        body="ABOUT"
-      />
-    );  
+      <div class = "about">
+      <form>
+      <div class = "info"> 
+        Contact Information <br></br>
+      Email Address: whoville@gmail.com <br></br>
+      Phone: 123-456-7890 <br></br>
+
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email address: </label>
+        <input type="email" class="form-control" id="exampleFormControlInput1"/>
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Comments:</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+   
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>  
+    </div>
+    );
   }
--- pubsafety-app/src/context.js
+++ pubsafety-app/src/context.js
@@ -22,4 +22,4 @@ function Card(props){
             </div>
         </div>
     );
-}
+};
