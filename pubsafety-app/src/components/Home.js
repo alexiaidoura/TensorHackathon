74dd0bee50374c55130ad5396cc6ceb90ac0a0f9
katelynmhopper <katelynmhopper@gmail.com>
2022-07-14 16:02:02 -0600

    finalized

--- pubsafety-app/src/components/Home.js
+++ pubsafety-app/src/components/Home.js
@@ -1,10 +1,8 @@
 //import {Home} from "./Home.js";
 //import React from 'react';
 //import NearMe from "./NearMe.js";
-
 //TODO: onClick={} -- add location to json file or just state? print history?
 // add a slider that you can use when you check in to say how busy the event is?
-
 function Home(){
   // const [status, setStatus] = React.useState('');
   // //const [show, setShow] = React.useState(true);
@@ -15,10 +13,8 @@ function Home(){
   // //check in: store current location and display last five?
   // //events near me: embed map with markers
   const mapwidth = 200;
-
   const [data, setData] = React.useState(null);        
     const [loaded, setLoaded] = React.useState(false);
-
     React.useEffect(() => {
         async function getData() {
             const response = await fetch('../src/components/eventdata.json');
@@ -30,57 +26,53 @@ function Home(){
         getData();
     },[])
     //console.log('loaded:', loaded, 'data:', data);
-
     
-
-
     return (
       <>
       <div className= "Home">
         
       <h1 className="display-2 text-center">Welcome to Whosville!</h1>
         
-      
       <div className="row">
-          <div className="col ">
-          <CardWide 
-              cardimage="../src/cotc-kids.jpeg"
-              header="Upcoming Events"
-              body={
-                <>
-                    {loaded && data.events.map((event,i) => (
+        <div className="col">
+        <CardWide 
+        cardimage="../src/cotc-kids.jpeg"
+        header="This week"
+        body={
+          <>
+            {loaded && data.events.map((event,i) => (
                     <MyEvent data={event} key={i}/>           
                 ))}     
-                </>  
-              }
-          />
-          </div> 
-        
-      
-        <div className="col">
-        <Card 
-          header="Check in!"
-          body= {
-          <>
-            <img src="../src/4641_w.jpeg" width="200"></img><br></br>
-            <button type="submit" className="btn btn-light">Check in!</button>
-
-          </>}
-        />
+        </>   
+        }
+      />
         </div>
         <div className="col">
         <Card 
           header="Events near me"
           body= {
           <>
-
           <a href='../src/components/nearme.html'><img src='../src/components/eventsnearme.png' style={{mapwidth}}></img></a>
           </>}
         />
         </div>
+      
+      
+      
+        <div className="col">
+        <Card 
+          header="Check in!"
+          body= {
+          <>
+            <img src="../src/4641_w.jpeg" width="200px"></img><br></br>
+            <button type="submit" className="btn btn-light">Check in!</button>
+          </>}
+        />
+        </div>
+ 
         </div>
         </div>
-
       </>
     );  
   }
+  //          <a href='../src/components/nearme.html'><img src='../src/components/nearmemap.png' width={mapwidth}></img></a>
--- pubsafety-app/src/components/MyEvent.js
+++ pubsafety-app/src/components/MyEvent.js
@@ -1,25 +1,9 @@
 function MyEvent({data}){
-    return (
-        <>
-            <h5>{data.event}</h5>
-            <h5>{data.date}</h5>
-            <h5>{data.location}</h5>
-            <hr></hr>
-
-                <table className="table">
-                  <tbody>
-                    <tr>
-                        <td className="text-black font-weight-bold">Event:</td>
-                        <td className="text-black">{data.event}</td>
-                    </tr>
-                    <tr>
-                        <td className="text-black font-weight-bold">Date:</td>
-                        <td className="text-black">{data.date}</td>
-                    </tr>
-                    
-                  </tbody>
-                </table>
-
-
-         </>
+  return (
+      <>
+          <h5>{data.event}</h5>
+          <h5>{data.date}</h5>
+          <h5>{data.location}</h5>
+          <hr></hr>
+       </>
 )};
