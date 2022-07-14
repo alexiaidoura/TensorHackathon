f313c6f22437b0b86ac27bf0b2ee9600c2cef22b
Alexia Idoura <alexia@idoura.com>
2022-07-14 06:22:57 -0400

    maps and added temp events page so app will load

---
+++ pubsafety-app/src/components/Events.js
@@ -0,0 +1,12 @@
+function Events(){
+  const ctx = React.useContext(UserContext);    
+  return (
+    <Card 
+      bgcolor="primary"
+      txtcolor="white"
+      header="EVENTS PLACEHOLDER"
+      title=""
+      text=""
+    />
+  )
+}
