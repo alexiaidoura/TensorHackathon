d60c4ae8f83c58d125ec6ac53547ae82366870d2
Alexia Idoura <alexia@idoura.com>
2022-07-13 20:41:58 -0400

    added create acct

--- pubsafety-app/src/index.js
+++ pubsafety-app/src/index.js
@@ -12,6 +12,7 @@ function Spa() {
       <NavBar/>
       <UserContext.Provider value={{users:[{name:'katelyn',email:'katelyn@example.com',password:'secret',lastlocation:'123 Main St'}]}}>              
         <Route path="/" exact component={Home} />
+        <Route path="/createaccount/" component={CreateAccount} />
         <Route path="/about" component={About} />
 
         <Route path="/connectUs" component={ConnectUs} />
