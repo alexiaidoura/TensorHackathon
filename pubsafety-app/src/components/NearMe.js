fd4a2e4bdde8d5bd9f86a9d3828304a69a1cdd2e
Alexia Idoura <alexia@idoura.com>
2022-07-14 10:01:50 -0400

    updating map, events on home

--- pubsafety-app/src/components/NearMe.js
+++ pubsafety-app/src/components/NearMe.js
@@ -12,7 +12,7 @@
 
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGlhcCIsImEiOiJjbDBwZGlxNm8xZHBpM2R0a2g3bXh2bmg2In0.Vwg8n-yrmyv8ZmV6WqEuWQ';
 
-    let map = new mapboxgl.Map({
+    var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-78.6382, 35.7796], // Raleigh NC
@@ -24,54 +24,58 @@
       .addTo(map);
 
       // This array will contain the bus stops pulled from city of raleigh data
-      let busStops = []; 
+      let myParks = []; 
     
       // main function that calls getStops, then adds the coordinates for each stop to the busStops array
       async function run() {
-      let stops = await getStops();
-      console.log('stops ' + stops);//this works
-      stops.forEach(element => {
-        console.log(element);
-        busStops.push([element.properties]);
-        
+      let parks = await getParks();
+      console.log('parks ' + parks);//this works
+      parks.forEach(element => { 
+        //HELP -- I WANT TO PULL OUT LAT AND LON. EACH ELEMENT LOOKS LIKE THIS:
+        //{"NAME":"Windemere Beaver Dam","LAT":35.81211,"LON":-78.67235} 
+        //console.log('thiselement ' + parks[key].attributes.LON,parks[key].attributes.LAT);
+        myParks.push([element.attributes.LON, element.attributes.LAT]);
+        console.log(myParks);
       });
       //console.log(busStops);
-      //FOR EACH BUS STOP, ADD A MARKER
-      // let marker2 = new mapboxgl.Marker()
-      // .setLngLat([element, 35.7796]) // TODO: #2 replace hardcoded point with first point from array 
-      // .addTo(map);
+      
   }
   
 
-  async function getStops(){
+  async function getParks(){
 
-    const url = 'https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/Parks/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
+    const url = 'https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/Parks/FeatureServer/0/query?where=1%3D1&outFields=NAME,LAT,LON&outSR=4326&f=json'
     const response = await fetch(url);
     const json = await response.json();
-    //console.log("json is:" + json);
-    let theStops = JSON.stringify(json);
+    //console.log("json is:" + JSON.stringify(json));
+    //let theParks = JSON.stringify(json);
+    let theParks = JSON.stringify(json); //HELP: NOT CONVINCED THIS SHOULD BE STRINGIFY -- I WANT TO STORE THE OBJECTS SO I CAN RETRIEVE ATTRIBUTES LON AND LAT LATER
 
     //let theStops = json.data;
-    //console.log("parks are:" + theStops);
-    return theStops;
+    console.log("parks are:" + theParks);
+    return theParks;
   }
 // counter here represents the index of the current bus stop
 let counter = 0;
-function move() {
+function addMarkers() {
   // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
   // Use counter to access bus stops in the array busStops
 
   setTimeout(() => {
-    if (counter >= busStops.length) return;
-    marker.setLngLat(busStops[counter]);
+    if (counter >= myParks.length) return;
+    console.log('counter ' + counter);
+    let marker2 = new mapboxgl.Marker()
+      .setLngLat(myParks[counter].LAT, myParks[counter].LON) // HELP:  
+      .addTo(map);
+    //marker.setLngLat(myParks[counter]);
     counter++;
-    move();
+    addMarkers();
   }, 1000);
 }
 
 run();
 
 
-// }
+ //}
 
 
